98bf95df205bae4a725afb49fcf659d0fc59925a
sibyl <fabriceyhc@gmail.com>
2023-02-02 19:44:20 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,17 @@
+from datasets import (
+    load_dataset,
+    load_from_disk
+)
 from transformers import (
-    AutoModelForSequenceClassification, 
-    AutoTokenizer, 
-    Trainer, 
-    TrainingArguments, 
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer,
     EarlyStoppingCallback
 )
+import evaluate
 
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-from datasets import load_dataset, load_from_disk
 import os
 import argparse
 import time
@@ -15,58 +19,49 @@ import torch
 import pandas as pd
 import random
 
-random.seed(130)
+from utils import *
 
-# aargparse
+random.seed(130)
+torch.use_deterministic_algorithms(False)
+
+# argparse
 
 parser = argparse.ArgumentParser(description='TextDiversity Trainer')
 
-parser.add_argument('--data-dir', type=str, default="./datasets/",
-                    help='path to data folders')
-parser.add_argument('--save-dir', type=str, default="./pretrained/",
-                    help='path to data folders')
-parser.add_argument('--num_epoch', default=10, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--train-batch-size', default=8, type=int, metavar='N',
-                    help='train batchsize')
-parser.add_argument('--eval-batch-size', default=16, type=int, metavar='N',
-                    help='eval batchsize')
-parser.add_argument('--gpus', default='0,1,2,3', type=str,
-                    help='id(s) for CUDA_VISIBLE_DEVICES')
-parser.add_argument('--num_runs', default=3, type=int, metavar='N',
-                    help='number of times to repeat the training')
 parser.add_argument('--techniques', nargs='+', 
-                    default=['fada', 'uniform', 'orig'],
+                    default=['orig', 'uniform', 'fada'],
                     type=str, help='technique used to generate paraphrases')
 parser.add_argument('--dataset-config', nargs='+', default=['glue', 'sst2'],
                     type=str, help='dataset info needed for load_dataset.')
 parser.add_argument('--dataset-keys', nargs='+', default=['text'],
                     type=str, help='dataset info needed for load_dataset.')
-parser.add_argument('--models', nargs='+',  default=['prajjwal1/bert-tiny', 'bert-base-uncased', 'bert-large-uncased'], 
+parser.add_argument('--models', nargs='+',  default=['prajjwal1/bert-tiny', 'bert-base-uncased'], 
                     type=str, help='pretrained huggingface models to train')
-parser.add_argument('--save-file', type=str, default='train_results.csv',
+parser.add_argument('--data-dir', type=str, default="./datasets/",
+                    help='path to data folders')
+parser.add_argument('--save-dir', type=str, default="./pretrained/",
+                    help='path to data folders')
+parser.add_argument('--num_epochs', default=10, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--logging_steps_per_epoch', default=10, type=int, metavar='N',
+                    help='number of times to run validation + log per epoch')
+parser.add_argument('--early_stopping_patience', default=10, type=int, metavar='N',
+                    help='number of times the validation metric can be lower before stopping training')
+parser.add_argument('--gradient_accumulation_steps', default=1, type=int, metavar='N',
+                    help='number of steps before updating the model weights')
+parser.add_argument('--train-batch-size', default=8, type=int, metavar='N',
+                    help='train batchsize')
+parser.add_argument('--eval-batch-size', default=16, type=int, metavar='N',
+                    help='eval batchsize') 
+parser.add_argument('--gpus', default='0,1,2,3', type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')
+parser.add_argument('--num_runs', default=3, type=int, metavar='N',
+                    help='number of times to repeat the training')
+parser.add_argument('--save-file', type=str, default='./results/train_results.csv',
                     help='name for the csv file to save with results')
 
 args = parser.parse_args()
 # print(args)
-
-
-#############################################################
-## Helper Functions #########################################
-#############################################################
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    acc = accuracy_score(labels, predictions.argmax(-1))
-    precision, recall, fbeta_score, support = precision_recall_fscore_support(
-        y_true=labels, 
-        y_pred=predictions.argmax(-1), 
-        average="weighted", 
-        zero_division=0)
-    return { 'accuracy': acc , 
-             'precision': precision, 
-             'recall': recall, 
-             'fbeta_score': fbeta_score} 
 
 #############################################################
 ## Main Loop Functionality ##################################
@@ -79,12 +74,12 @@ def train(args):
 
     run_args = []
     for run_num in range(args.num_runs):
-        for MODEL_NAME in args.models:
+        for model in args.models:
             for technique in args.techniques:
                 run_args.append({
                     "run_num":run_num,
                     "technique":technique,
-                    "MODEL_NAME":MODEL_NAME,
+                    "model":model,
                 })
 
     print(run_args)
@@ -105,7 +100,7 @@ def train(args):
         #############################################################
         run_num = run_arg['run_num']
         technique = run_arg['technique']
-        MODEL_NAME = run_arg['MODEL_NAME']
+        model_checkpoint = run_arg['model']
 
         print(pd.DataFrame([run_arg]))
 
@@ -115,77 +110,71 @@ def train(args):
             # if not 1 then assume 2 keys
             sentence1_key, sentence2_key = args.dataset_keys
 
-        def tokenize_fn(batch):
-            if sentence2_key is None:
-                return tokenizer(batch[sentence1_key], padding=True, truncation=True, max_length=250)
-            return tokenizer(batch[sentence1_key], batch[sentence2_key], padding=True, truncation=True, max_length=250)
-
         #############################################################
         ## Dataset Preparation ######################################
         #############################################################
 
         dataset_name = args.dataset_config[0]
 
-        if technique == "orig":
-            train_dataset = load_dataset(*args.dataset_config, split='train').shuffle()
-            if 'sst2' in args.dataset_config:
-                train_dataset  = train_dataset.rename_column("sentence", "text")
-        else:
+        raw_datasets = load_dataset(*args.dataset_config)
+        if 'sst2' in args.dataset_config:
+            raw_datasets.pop("test") # test set is not usable (all labels -1)
+            raw_datasets["train"] = raw_datasets["train"].select(range(1000))
+
+        if technique != "orig":
             save_name = ".".join(args.dataset_config) + ".sibyl." + technique
             save_path = os.path.join(args.data_dir, save_name)
             train_dataset = load_from_disk(save_path)
-            print(train_dataset)
+            raw_datasets["train"] = train_dataset
 
-        if 'sst2' in args.dataset_config:
-            # special handling since sst2 has no test labels
-            dataset_name = args.dataset_config[1]
-            eval_dataset  = load_dataset(*args.dataset_config, split='validation')
-            eval_dataset  = eval_dataset.rename_column("sentence", "text")
-            test_valid    = eval_dataset.train_test_split(test_size=0.5)
-            eval_dataset  = test_valid['test']
-            test_dataset  = test_valid['train']
-        else:
-            eval_dataset  = load_dataset(*args.dataset_config, split='validation')
-            test_dataset  = load_dataset(*args.dataset_config, split='test')
+        raw_datasets = prepare_splits(raw_datasets)
+        raw_datasets = rename_text_columns(raw_datasets)
+        raw_datasets = raw_datasets.shuffle(seed=run_num)
 
-        # shuffle the dataset
-        train_dataset = train_dataset.shuffle(seed=run_num)
-        num_classes = train_dataset.features['label'].num_classes
+        num_labels = len(raw_datasets["train"].features["label"].names)
 
-        print('Length of train_dataset:', len(train_dataset))
-        print('Length of eval_dataset:', len(eval_dataset))
-        print('Length of test_dataset:', len(test_dataset))
-        print('Number of classes:', num_classes)
+        print(raw_datasets)
+        print('Number of classes:', num_labels)
 
         #############################################################
         ## Model + Tokenize #########################################
         #############################################################
-        checkpoint = args.save_dir + MODEL_NAME + '-' + "_".join(args.dataset_config) + "_" + technique
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-        model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=num_classes).to(device)
+        
+        checkpoint = args.save_dir + model_checkpoint + '-' + "_".join(args.dataset_config) + "_" + technique
+        tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+        model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels).to(device)
 
         # tokenize datasets
-        train_dataset = train_dataset.map(tokenize_fn, batched=True, batch_size=1000)
-        train_dataset = train_dataset.rename_column("label", "labels") 
-        train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+        def preprocess_function(batch):
+            if sentence2_key is None:
+                return tokenizer(batch[sentence1_key], padding=True, truncation=True)
+            return tokenizer(batch[sentence1_key], batch[sentence2_key], padding=True, truncation=True)
 
-        eval_dataset = eval_dataset.map(tokenize_fn, batched=True, batch_size=len(eval_dataset))
-        eval_dataset = eval_dataset.rename_column("label", "labels") 
-        eval_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+        tokenized_datasets = raw_datasets.map(preprocess_function, batched=True)
 
-        test_dataset = test_dataset.map(tokenize_fn, batched=True, batch_size=len(test_dataset))
-        test_dataset = test_dataset.rename_column("label", "labels") 
-        test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+        #############################################################
+        ## Metrics ##################################################
+        #############################################################
+
+        metrics = evaluate.combine([
+            evaluate.load('accuracy'), 
+            ConfiguredMetric(evaluate.load('precision'), average='weighted'),
+            ConfiguredMetric(evaluate.load('recall'), average='weighted'),
+            ConfiguredMetric(evaluate.load('f1'), average='weighted'),
+        ])
+
+        def compute_metrics(eval_pred):
+            preds, labels = eval_pred
+            preds = np.argmax(preds, axis=1)
+            return metrics.compute(predictions=preds, references = labels)
 
         #############################################################
         ## Callbacks ################################################
         #############################################################
 
         callbacks = []
-
-        tmcb = None   
         escb = EarlyStoppingCallback(
-            early_stopping_patience=5
+            early_stopping_patience=args.early_stopping_patience
         )
         callbacks.append(escb)
 
@@ -193,12 +182,8 @@ def train(args):
         ## Training  ################################################
         #############################################################
 
-        train_batch_size = args.train_batch_size    
-        eval_batch_size = args.eval_batch_size  
-        num_epoch = args.num_epoch  
-        gradient_accumulation_steps = 1
-        max_steps = int((len(train_dataset) * num_epoch / gradient_accumulation_steps) / train_batch_size)
-        logging_steps = max_steps // num_epoch
+        max_steps = (len(tokenized_datasets["train"]) * args.num_epochs // args.gradient_accumulation_steps) // args.train_batch_size
+        logging_steps = (max_steps // args.num_epochs) // args.logging_steps_per_epoch
 
         training_args = TrainingArguments(
             output_dir=checkpoint,
@@ -206,28 +191,30 @@ def train(args):
             max_steps=max_steps,
             save_steps=logging_steps,
             save_total_limit=1,
-            per_device_train_batch_size=train_batch_size,
-            per_device_eval_batch_size=eval_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps, 
-            warmup_steps=int(max_steps / 10),
+            per_device_train_batch_size=args.train_batch_size,
+            per_device_eval_batch_size=args.eval_batch_size,
+            gradient_accumulation_steps=args.gradient_accumulation_steps, 
+            warmup_steps=logging_steps,
+            learning_rate=2e-5,
             weight_decay=0.01,
             logging_dir='./logs',
             logging_steps=logging_steps,
             logging_first_step=True,
             load_best_model_at_end=True,
-            metric_for_best_model="accuracy",
-            greater_is_better=True,
+            metric_for_best_model="loss",
             evaluation_strategy="steps",
-            remove_unused_columns=False
         )
+
+        data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
         trainer = Trainer(
             model=model, 
             tokenizer=tokenizer,
             args=training_args,
             compute_metrics=compute_metrics,                  
-            train_dataset=train_dataset,         
-            eval_dataset=eval_dataset,
+            train_dataset=tokenized_datasets["train"],         
+            eval_dataset=tokenized_datasets["validation"],
+            data_collator=data_collator,
             callbacks=callbacks
         )
 
@@ -236,24 +223,25 @@ def train(args):
         run_time = time.time() - start_time
 
         # test with ORIG data
-        trainer.eval_dataset = test_dataset
-
-        out = trainer.evaluate()
-
-        out['run_name'] = checkpoint
-        out['model_name'] = MODEL_NAME
-        out['run_num'] = run_num
-        out['technique'] = technique
-        out['dataset_size'] = len(train_dataset)
-        out['dataset_config'] = args.dataset_config
-        out['run_time'] = run_time
+        out = trainer.evaluate(tokenized_datasets["test"])
+        out.update({
+            "run_num":          run_num,
+            "technique":        technique,
+            "model_checkpoint": model_checkpoint,
+            "checkpoint":       checkpoint,
+            "dataset_config":   args.dataset_config,
+            "train_size":       len(tokenized_datasets["train"]),
+            "valid_size":       len(tokenized_datasets["validation"]),
+            "test_size":        len(tokenized_datasets["test"]),
+            "run_time":         run_time,
+        })
         print('Performance of {}\n{}'.format(checkpoint, out))
 
         results.append(out)
 
         # save results
         df = pd.DataFrame(results)
-        df.to_csv(save_file)
+        df.to_csv(save_file, index=False)
 
 
 if __name__ == "__main__":
