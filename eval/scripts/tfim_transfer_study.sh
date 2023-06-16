b5ea0875ef31ca408ae0a578ba0b5f2934f8788a
sibyl <fabriceyhc@gmail.com>
2023-06-15 17:26:56 -0700

    Update tfim_transfer_study.sh

--- eval/scripts/tfim_transfer_study.sh
+++ eval/scripts/tfim_transfer_study.sh
@@ -36,7 +36,10 @@ python -m eval.train dataset_dir=./fada/fadata/transfer_datasets \
                      dataset.config_name=sst2 \
                      train.save_path=./eval/results/glue.sst2.training-transfer.csv
 
-python -m eval.robustness robustness.model_matcher=./eval/pretrained/*glue.sst2.*tfim* \
+python -m eval.robustness dataset_dir=./fada/fadata/transfer_datasets \
+                          dataset.builder_name=glue \
+                          dataset.config_name=sst2 \
+                          robustness.model_matcher=./eval/pretrained/*glue.sst2.*tfim* \
                           robustness.save_path=./eval/results/glue.sst2.robustness-transfer.csv
 
 # imdb.plain_text --> others
@@ -72,7 +75,10 @@ python -m eval.train dataset_dir=./fada/fadata/transfer_datasets \
                      dataset.config_name=plain_text \
                      train.save_path=./eval/results/imdb.plain_text.training-transfer.csv
 
-python -m eval.robustness robustness.model_matcher=./eval/pretrained/*imdb.plain_text.*tfim* \
+python -m eval.robustness dataset_dir=./fada/fadata/transfer_datasets \
+                          dataset.builder_name=imdb \
+                          dataset.config_name=plain_text \
+                          robustness.model_matcher=./eval/pretrained/*imdb.plain_text.*tfim* \
                           robustness.save_path=./eval/results/imdb.plain_text.robustness-transfer.csv
 
 # ag_news.default --> others
@@ -108,7 +114,10 @@ python -m eval.train dataset_dir=./fada/fadata/transfer_datasets \
                      dataset.config_name=default \
                      train.save_path=./eval/results/ag_news.default.training-transfer.csv
 
-python -m eval.robustness robustness.model_matcher=./eval/pretrained/*ag_news.default.*tfim* \
+python -m eval.robustness dataset_dir=./fada/fadata/transfer_datasets \
+                          dataset.builder_name=ag_news \
+                          dataset.config_name=default \
+                          robustness.model_matcher=./eval/pretrained/*ag_news.default.*tfim* \
                           robustness.save_path=./eval/results/ag_news.default.robustness-transfer.csv
 
 # yahoo_answers_topics.yahoo_answers_topics --> others
@@ -144,6 +153,9 @@ python -m eval.train dataset_dir=./fada/fadata/transfer_datasets \
                      dataset.config_name=yahoo_answers_topics \
                      train.save_path=./eval/results/yahoo_answers_topics.yahoo_answers_topics.training-transfer.csv
 
-python -m eval.robustness robustness.model_matcher=./eval/pretrained/*yahoo_answers_topics.yahoo_answers_topics.*tfim* \
+python -m eval.robustness dataset_dir=./fada/fadata/transfer_datasets \
+                          dataset.builder_name=yahoo_answers_topics \
+                          dataset.config_name=yahoo_answers_topics \
+                          robustness.model_matcher=./eval/pretrained/*yahoo_answers_topics.yahoo_answers_topics.*tfim* \
                           robustness.save_path=./eval/results/yahoo_answers_topics.yahoo_answers_topics.robustness-transfer.csv
 
